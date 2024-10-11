f1f36662ca54f51cfe425f61b80b140bcbc89f1a
Wanja-Kayago <kayagonaomiwanja@gmail.com>
2024-10-11 18:00:55 +0300

    changed sidebar

--- totosteps-dashboard/src/app/components/Sidebar/index.tsx
+++ totosteps-dashboard/src/app/components/Sidebar/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
+
 interface NavItemProps {
   href: string;
   icon: React.ReactElement;
